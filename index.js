28ff7c3b34c766b886defe2199a0962de85f4190
Jonathan Oaklyn <jonathan.revell.hopkins@gmail.com>
2021-12-14 10:44:26 -0700

    Install, clean and recursive pack

--- index.js
+++ index.js
@@ -8,24 +8,25 @@ var path = require('path');
 var defaults = require('defaults');
 
 var c = require('./config');
+const { coerceLocalModulesDirectory } = require('./lib/config-controller');
 
 // require all commands located in "./lib" folder
 var commands = (function requireCommands(){
 
-  var cmds = {};
-  var p = path.join(__dirname, 'lib', 'commands');
+    var cmds = {};
+    var p = path.join(__dirname, 'lib', 'commands');
 
-  try {
-    var scripts = fs.readdirSync(p);
-    scripts.forEach(function(script){
-      var cmd = path.basename(script, '.js');
-      cmds[cmd] = require(path.join(p, script));
-    });
-  } catch(e) {
-    console.error('could not read command scripts', e.message);
-  }
+    try {
+        var scripts = fs.readdirSync(p);
+        scripts.forEach(function(script){
+            var cmd = path.basename(script, '.js');
+            cmds[cmd] = require(path.join(p, script));
+        });
+    } catch(e) {
+        console.error('could not read command scripts', e.message);
+    }
 
-  return cmds;
+    return cmds;
 
 })();
 
@@ -35,70 +36,93 @@ var commands = (function requireCommands(){
  * --dir {String} local modules directory name
  */
 module.exports = function localModules(o) {
-  /**
+    /**
    * get options right, the options object is used as a `blackboard` as well. lookup: blackboard design pattern.
    */
-  var options = defaults({}, o);
-  defaults(options, c);
+    var options = defaults({}, o);
+    defaults(options, c);
 
-  /**
+    /**
    *  early return commands
    */
-  if (options.help || options.h || options.H) return commands.help(options);
-  if (options.version || options.v || options.V) return commands.version(options);
+    if (options.help || options.h || options.H) return commands.help(options);
+    if (options.version || options.v || options.V) return commands.version(options);
 
-  /**
+    /**
    * arguments, alias, defaults etc.
    */
-  options.cmd = options.cmd || options._[0] || '';
-  options.force = options.force || options.f;
-  options.modules = [];
+    options.cmd = options.cmd || options._[0] || '';
+    options.force = options.force || options.f;
+    options.modules = [];
 
-  /**
+    /**
    * basic processing: read local_modules and package.json
    */
-  options.dirPath = path.join(process.cwd(), options.dir);
-  options.packagePath = path.join(process.cwd(), options.package);
+    options.dirPath = path.join(process.cwd(), options.dir);
+    options.packagePath = path.join(process.cwd(), options.package);
 
-  // read package.json
-  options.packageJSON = fs.readFileSync(options.packagePath, 'utf8');
+    coerceLocalModulesDirectory(options);
 
-  // parse package.json
-  options.pkg = JSON.parse(options.packageJSON);
-  options.pkg.dependencies = options.pkg.dependencies || {};
+    // read package.json
+    options.packageJSON = fs.readFileSync(options.packagePath, 'utf8');
 
-  // try to read local_modules directory
-  var directories = [];
-  try {
-    directories = fs.readdirSync(options.dirPath);
-  } catch (e) {
-    console.error('could not read local module directory: ' + e.message);
-  }
+    // parse package.json
+    options.pkg = JSON.parse(options.packageJSON);
+    options.pkg.dependencies = options.pkg.dependencies || {};
 
-  // remove non directories
-  options.modules = directories.filter(function(entry) {
-      var dir = path.join(options.dirPath, entry);
-      return fs.statSync(dir).isDirectory();
-    });
+    // try to read local_modules directory
+    options.modules = resolveModules(options.dirPath);
 
-  /**
+    /**
    * handle commands
    */
-  var executed = Object.keys(commands).some(function(command){
-    if (options.cmd == command || options[command]) {
-      commands[command](options);
-      return true;
-    }
-  });
+    var executed = Object.keys(commands).some(function(command){
+        if (options.cmd == command || options[command]) {
+            commands[command](options);
+            return true;
+        }
+    });
 
-  if (!executed) console.log('no command provided');
+    if (!executed) console.log('no command provided');
 
 };
+
+function resolveModules(baseDirectory) {
+        // try to read a local_module directory or subdirectory
+        var directories = [];
+        var initialResults = [];
+        var modules = [];
+        try {
+            initialResults = fs.readdirSync(baseDirectory);
+        } catch (e) {
+            console.error('could not read local module directory: ' + e.message);
+        }
+
+        // remove non directories
+        directories = initialResults.filter(function(entry) {
+                var dir = path.join(baseDirectory, entry);
+                return fs.statSync(dir).isDirectory();
+        });
+
+        directories.forEach(dir => {
+            // Resolve Scoped Modules
+            if(dir.indexOf("@") >= 0) {
+                var scopedModules = resolveModules(path.resolve(baseDirectory, dir));
+                scopedModules.forEach(sDir => {
+                    modules.push(`${dir}/${sDir}`);
+                });
+            } else {
+                modules.push(dir)
+            }
+        });
+
+        return modules;
+}
 
 /**
  * run from command line
  */
 if (require.main === module) {
-  var args = require('subarg')(process.argv.slice(2));
-  module.exports(args);
+    var args = require('subarg')(process.argv.slice(2));
+    module.exports(args);
 }
